b6ce94a13571aa175b1fff4b0e29297b83cfb47e
GabrielMerigo <gabriel.merigo.dev@gmail.com>
2022-11-09 23:21:21 -0300

    new bundle

--- android/app/src/main/java/com/myskills/MainActivity.java
+++ android/app/src/main/java/com/myskills/MainActivity.java
@@ -4,21 +4,21 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
-public class MainActivity extends ReactActivity {
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "myskills";
   }
 
-  /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
-   * you can specify the rendered you wish to use (Fabric or the older renderer).
-   */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
